cfc319e68a38255ce012b2fe3b6704c38f5c8f62
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2024-06-16 20:00:43 +0700

    update composition

--- src/Composition/Motherboard.java
+++ src/Composition/Motherboard.java
@@ -1,2 +1,23 @@
-package Composition;public class Motherboard {
+package Composition;
+
+//Motherboard got an IS-A relationship with Product, meaning that Motherboard is a type of product.
+public class Motherboard extends Product{
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        super(model, manufacturer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName){
+        System.out.println("Program "+ programName +" is now loading....");
+    }
 }
