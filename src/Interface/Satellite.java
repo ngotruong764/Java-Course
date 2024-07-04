8e657b73b8acb5fe50cf49dd1077c3e8035e494b
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2024-07-04 20:43:39 +0700

    update interface

--- src/Interface/Satellite.java
+++ src/Interface/Satellite.java
@@ -16,4 +16,13 @@ public class Satellite implements OrbitEarth{
 
     }
 
+    @Override
+    public void achieveOrbit() {
+
+    }
+
+    @Override
+    public FlightStages transition(FlightStages stage) {
+        return OrbitEarth.super.transition(stage);
+    }
 }
